33b4c8ff7df5fcc45522bccf15d24125dae04f60
Rahul-Rocky <80695794+Rahul-Rocky@users.noreply.github.com>
2021-08-31 22:34:06 +0530

    Add files via upload

---
+++ KCG/src/basic/Size_of_a_variable.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Size_of_a_variable {
+
+	public static void main(String[] args) {
+		
+		    System.out.println("Size of byte: " + (Byte.SIZE/8) + " bytes.");
+		    System.out.println("Size of short: " + (Short.SIZE/8) + " bytes.");
+		    System.out.println("Size of int: " + (Integer.SIZE/8) + " bytes.");
+		    System.out.println("Size of long: " + (Long.SIZE/8) + " bytes.");
+		    System.out.println("Size of char: " + (Character.SIZE/8) + " bytes.");
+		    System.out.println("Size of float: " + (Float.SIZE/8) + " bytes.");
+		    System.out.println("Size of double: " + (Double.SIZE/8) + " bytes.");
+
+	}
+
+}
---
+++ KCG/src/basic/Variables.java
@@ -0,0 +1,16 @@
+	package basic;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a=1;
+		float f=1.0f;
+		double b=2.0d;
+		char c='v';
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
